4d60d466c2aeddd8426990e37b06b6384a8711f8
Maxscout <doctorrooskie1@gmail.com>
2022-08-06 15:24:30 -0400

    make window with button

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,27 @@
-use glib::clone;
-// glib and other dependencies are re-exported by the gtk crate
-use gtk::glib;
 use gtk::prelude::*;
-
-// When the application is launched…
-fn on_activate(application: &gtk::Application) {
-    // … create a new window …
-    let window = gtk::ApplicationWindow::new(application);
-    // … with a button in it …
-    let button = gtk::Button::with_label("Hello World!");
-    // … which closes the window when clicked
-    button.connect_clicked(clone!(@weak window => move |_| window.close()));
-    window.set_child(Some(&button));
-    window.present();
-}
+use gtk::{Application, ApplicationWindow, Button};
 
 fn main() {
-    // Create a new application with the builder pattern
-    let app = gtk::Application::builder()
-        .application_id("com.github.gtk-rs.examples.basic")
+    let application = Application::builder()
+        .application_id("com.example.FirstGtkApp")
         .build();
-    app.connect_activate(on_activate);
-    // Run the application
-    app.run();
+
+    application.connect_activate(|app| {
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .title("First GTK Program")
+            .default_width(350)
+            .default_height(70)
+            .build();
+
+        let button = Button::with_label("Click me!");
+        button.connect_clicked(|_| {
+            eprintln!("Clicked!");
+        });
+        window.add(&button);
+
+        window.show_all();
+    });
+
+    application.run();
 }
